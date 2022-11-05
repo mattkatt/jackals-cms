66897dba0760e267bbab4c5c2b737998854b4a91
mattkatt <m4ttk4tt@hotmail.co.uk>
2022-11-05 16:02:12 +0000

    build(settings): added secret_key from .env

--- main/settings/production.py
+++ main/settings/production.py
@@ -1,6 +1,14 @@
+import dotenv
+
 from .base import *
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 try:
     from .local import *
