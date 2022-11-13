e065ff4b37ec203691cc6cfc301fab816c658455
mattkatt <m4ttk4tt@hotmail.co.uk>
2022-11-13 20:04:11 +0000

    fix(pages): only display live pages

--- main/helper_methods.py
+++ main/helper_methods.py
@@ -5,7 +5,7 @@ from wagtail.models import Page
 def append_sidebar_to_context(context):
     try:
         events: Page = apps.get_model(app_label='events', model_name='EventPage')
-        latest_event = events.objects.filter(live=True).latest('event_date')
+        latest_event = events.objects.live().latest('event_date')
     except Page.DoesNotExist:
         latest_event = None
 
@@ -13,7 +13,7 @@ def append_sidebar_to_context(context):
 
     try:
         library_index = apps.get_model(app_label='library', model_name='LibraryIndexPage')
-        library_first_level_children = library_index.objects.first().get_children()
+        library_first_level_children = library_index.objects.first().get_children().live()
     except Page.DoesNotExist:
         library_first_level_children = None
 
