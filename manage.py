6888daecad3db5c796b9ba46448e4d2bf0bda29a
mattkatt <m4ttk4tt@hotmail.co.uk>
2022-11-14 20:49:27 +0000

    fix(manage.py): use production settings if IS_DEV is False

--- manage.py
+++ manage.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
+
+dotenv_file = os.path.join(".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings.dev")
+    if os.environ['IS_DEV']:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings.dev")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings.production")
 
     from django.core.management import execute_from_command_line
 
