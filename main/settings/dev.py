d6e144ac2fcd02a80e459381d3f4933c21ca38c4
mattkatt <m4ttk4tt@hotmail.co.uk>
2022-11-14 20:29:13 +0000

    feat(db): switch to mysql in production

--- main/settings/dev.py
+++ main/settings/dev.py
@@ -19,6 +19,16 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 try:
     from .local import *
 except ImportError:
--- main/settings/production.py
+++ main/settings/production.py
@@ -19,6 +19,19 @@ ALLOWED_HOSTS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': f'{os.environ["DB_NAME"]}',
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+    }
+}
+
 try:
     from .local import *
 except ImportError:
